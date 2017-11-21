793449a9e1b4480d46a551c8c15c8d01efad9ff1
KatyaSeotlt <Katya@seotlt.ru>
2017-11-21 15:21:38 +0400

    all

--- js/index.js
+++ js/index.js
@@ -21,12 +21,17 @@ var app = {
     username: "seotlt",
     password: "newagspass",
     registrationId: "mytest",
+    detail: false,
     initialize: function() {
         this.bindEvents();
 
     },
     isAuth: function() {
+  //    console.log(window.localStorage.getItem("access"));
       return !(window.localStorage.getItem("access") === undefined || window.localStorage.getItem("access") === "undefined" || window.localStorage.getItem("access") === null);
+    },
+    isLocal: function(name){
+      return !(window.localStorage.getItem(name) === undefined || window.localStorage.getItem(name) === "undefined" || window.localStorage.getItem(name) === null);
     },
     // Bind Event Listeners
     //
@@ -54,10 +59,16 @@ var app = {
                 },
                 dataType: "json",
                 contentType: "application/javascript",
-                data: myData,
+                data:  JSON.stringify(myData),
                 url: "http://seotlt:newagspass@crypto.seotlt.ru/newnext/mobile.php",
                 success: function (jsonData) {
-                    console.log(jsonData);
+                  if(jsonData.error==='false'){
+                    $('#page-sms-confirm').show();
+                    $('#page-signals').hide();
+                    $('#page-registr').hide();
+                  }else{
+                    $('.phoneError').html(msg.error);
+                  }
                 },
                 error: function (request, textStatus, errorThrown) {
                     console.log(request.responseText);
@@ -81,11 +92,19 @@ var app = {
                 },
                 dataType: "json",
                 contentType: "application/javascript",
-                data: myData,
+                data: JSON.stringify(myData),
                 url: "http://seotlt:newagspass@crypto.seotlt.ru/newnext/mobile.php",
                 success: function (jsonData) {
-                    console.log(jsonData);
+                  if(jsonData.error==='false'){
+                    $('#page-sms-confirm').hide();
+                    $('#page-signals').show();
+                    $('#page-registr').hide();
+                    app.getDataSignals();
                     window.localStorage.setItem("access", 1);
+                  }else{
+                    $('.signalsError').html(msg.error);
+                  }
+
                 },
                 error: function (request, textStatus, errorThrown) {
                     console.log(request.responseText);
@@ -95,6 +114,21 @@ var app = {
                 username: app.username,
                 password: app.password,
             });
+    },
+    showDetail: function(id){
+      if(app.detail==false){
+        app.detail=true;
+      $('.signalblock').hide();
+      $('#signal'+id).addClass('showDetail');
+      $('#signal'+id).show();
+      $('#signal'+id+' textLine1 span').removeClass('blueRound');
+      localStorage.setItem('signal'+id, 'show');
+    }else{
+      app.detail=false;
+    $('#signal'+id).removeClass('showDetail');
+    $('#signal'+id+' textLine1 span').removeClass('blueRound');
+    $('.signalblock').show();
+    }
     },
     //getData Signal
     getDataSignals:  function(){
@@ -108,12 +142,60 @@ var app = {
                 },
                 dataType: "json",
                 contentType: "application/javascript",
-                data: myData,
+                data:  JSON.stringify(myData),
                 url: "http://seotlt:newagspass@crypto.seotlt.ru/newnext/mobile.php",
                 success: function (jsonData) {
                     console.log(jsonData);
                     var html='';
-                      $('#page-signals').html(html);
+                    var arrow_type = 'img/arrow_up.png';
+                    var color_type = 'greentext';
+                    var showSignalsCircle='';
+
+                    for (var i in jsonData.signals){
+                       arrow_type = 'img/arrow_up.png';
+                       color_type = 'greentext';
+                      if(jsonData.signals[i].type=='SHORT'){
+                        arrow_type = 'img/arrow_d.png';
+                        color_type = 'redtext';
+                        }
+                    var shareOptions = {
+                        title: "SGA cryptotrading Signals",
+                        message: jsonData.signals[i].dates+ " "+jsonData.signals[i].times+". "+jsonData.signals[i].doit+" позицию "+jsonData.signals[i].type+". Криптовалютная пара " +jsonData.signals[i].name+ ". Рекомендованная цена "+jsonData.signals[i].price,
+                        url: "http://sga.com.ru/sgacryptotrading.php"
+                      };
+                      var url="https://vk.com/share.php?title="+shareOptions.message+" http://sga.com.ru/sgacryptotrading.php";
+                      var okurl="http://ok.ru/dk?st.cmd=addShare&st.s=1&st._surl=http://sga.com.ru/sgacryptotrading.php&st.comments="+shareOptions.message;
+                      var twurl="http://twitter.com/share?text="+shareOptions.message+"&url=http://sga.com.ru/sgacryptotrading.php";
+                      var fburl="http://www.facebook.com/share.php?u=http://sga.com.ru/sgacryptotrading.php";
+                      var fburl2="https://www.facebook.com/dialog/feed?app_id=145634995501895&display=popup&amp;caption="+shareOptions.message+"&link=http://sga.com.ru/sgacryptotrading.php&redirect_uri=http://sga.com.ru/sgacryptotrading.php";
+
+
+                      if(!app.isLocal('signal'+jsonData.signals[i].id)){showSignalsCircle='blueRound';}
+                      html=html+'<div class="signalblock" id="signal'+jsonData.signals[i].id+'" onclick="app.showDetail('+jsonData.signals[i].id+')">'
+                      +'<div class="textLine1"><span class="'+showSignalsCircle+'"></span>'+jsonData.signals[i].doit+' позицию</div>'
+                      +'<div class="timeLines">'
+                        +'<div class="dates"><img src="img/calendar.png" class="imgStyle"/><span class="normalTextBig">'+jsonData.signals[i].dates+'</span> </div>'
+                        +'<div class="dates"><img src="img/time.png" class="imgStyle"/><span class="normalTextBig">'+jsonData.signals[i].times+'</span> </div>'
+                      +'</div>'
+                      + '<div class="openPosition"><span class="normalTextBig">'+jsonData.signals[i].doit+' позицию </span>'
+                      + '<div class="'+color_type+'">'+jsonData.signals[i].type+'</div><img src="'+arrow_type+'" class="imgStyleArrow"/></div>'
+                      + '<div class="price"><div class="litetext">Криптовалютная пара</div></div>'
+                      + '<div class="tiker"><div class="normalTextBigEMitent">'+jsonData.signals[i].name+'</div></div>'
+                      + '<div class="price"><div class="litetext">Рекомендованная цена</div></div>'
+                      + '<div class="tiker"><div class="normalTextBig">'+jsonData.signals[i].price+'</div></div>'
+                      + '<div class="detail price"><div class="litetext">Поделиться</div></div>'
+                      + '<div class="detail tiker">'
+                      +'<div class="shareicons"><a href="'+url+'" target="_system" data-rel="external"><img src="img/vk.png" class="shareicon"/></a></div>'
+                      +'<div class="shareicons"><a href="'+twurl+'" target="_system" data-rel="external"><img src="img/tw.png" class="shareicon"/></a></div>'
+                      +'<div class="shareicons"><a href="'+okurl+'" target="_system" data-rel="external"><img src="img/ok.png" class="shareicon"/></a></div>'
+                      +'</div>'
+                      +'</div>';
+                    }
+                    if(html==''){
+                      html='<div class="nodata"><div class="normalTextBig">Сигналов не обнаружено</div></div>';
+                    }
+                    $('.signal-container').html(html);
+                    window.setTimeout(function(){  app.getDataSignals();}, 10000);
                 },
                 error: function (request, textStatus, errorThrown) {
                     console.log(request.responseText);
@@ -130,12 +212,13 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.setupPush();
-        this.registrationId=localStorage.getItem('registrationId');
+        app.registrationId=localStorage.getItem('registrationId');
         if(app.isAuth()){
           $('#page-registr').hide();
           $('#page-sms-confirm').hide();
           app.getDataSignals();
           $('#page-signals').show();
+          $('body').addClass('open');
         }else{
           $('#page-sms-confirm').hide();
           $('#page-signals').hide();
@@ -188,6 +271,7 @@ var app = {
                 data.title,           // title
                 'Ok'                  // buttonName
             );
+             app.getDataSignals();
        });
     }
 };
